cc8eeeb677bd33b4909c05d773f074bf17480715
clxxiii <clxxiii@users.noreply.github.com>
2023-06-17 22:00:52 -0400

    preview load image

--- src/routes/(server-functions)/preview/[id]/+page.svelte
+++ src/routes/(server-functions)/preview/[id]/+page.svelte
@@ -3,6 +3,7 @@
 	import ChartPreviewer from '$lib/components/ChartPreviewer.svelte';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
+	import Background from '$lib/assets/preview-load-bg.png';
 
 	export let data;
 	const { chart } = data;
@@ -28,8 +29,38 @@
 		/>
 	</div>
 {:else}
-	<h2>Loading Preview...</h2>
+	<div class="loading" transition:fade={{ duration: 200 }}>
+		<h2>Loading Preview</h2>
+		<img src={Background} alt="" />
+	</div>
 {/if}
 
 <style>
+	.loading {
+		position: absolute;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+	}
+	.loading h2 {
+		background-color: rgba(0, 0, 0, 0.75);
+		padding: 10px;
+		font-size: 36px;
+		font-family: 'Fira Code';
+		color: white;
+		z-index: 2;
+	}
+	.loading img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		filter: blur(5px);
+	}
 </style>
